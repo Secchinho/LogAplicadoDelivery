8a9ca6ed4d4bb13bda0375bbfd566ccb7431e86a
Raphael Figueiredo Secchin <raphasec@gmail.com>
2026-06-01 11:33:43 -0300

    Começo da criação do pacote

--- src/main/java/com/ufes/delivery/repository/ICupomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ufes.delivery.repository;
-
-import com.ufes.delivery.model.CupomDescontoPedido;
-import java.util.Optional;
-
-public interface ICupomRepository {
-    Optional<CupomDescontoPedido> buscarCupom(String codigo);
-}
